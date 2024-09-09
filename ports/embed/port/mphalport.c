baf1ad27b4b8296202f3fa1d7c2ebdba889b28f7
Nils Hoffmann <82971538+NiHoffmann@users.noreply.github.com>
2024-09-09 12:32:46 +0200

    pip std out though monitor helper function

--- ports/embed/port/mphalport.c
+++ ports/embed/port/mphalport.c
@@ -26,8 +26,15 @@
 
 #include <stdio.h>
 #include "py/mphal.h"
+#ifdef ORB_ENABLE_MONITOR_STD_OUT
+    #include "monitor.h"
+#endif
 
 // Send string of given length to stdout, converting \n to \r\n.
 void mp_hal_stdout_tx_strn_cooked(const char *str, size_t len) {
-    printf("%.*s", (int)len, str);
+    #ifdef ORB_ENABLE_MONITOR_STD_OUT
+        setMonitorText(str, len);
+    #else
+        printf("%.*s", (int)len, str);
+    #endif
 }
